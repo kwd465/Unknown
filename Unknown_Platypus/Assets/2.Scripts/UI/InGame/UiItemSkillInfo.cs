c76d17384ef2750534b691ddee9a08b913ce16ed
kwd465 <kwd465@gmail.com>
2024-12-04 19:45:32 +0900

    이펙트 활성화 로직

--- Unknown_Platypus/Assets/2.Scripts/UI/InGame/UiItemSkillInfo.cs
+++ Unknown_Platypus/Assets/2.Scripts/UI/InGame/UiItemSkillInfo.cs
@@ -1,3 +1,5 @@
+using Coffee.UIExtensions;
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,15 +13,21 @@ public class UiItemSkillInfo : UIBase
     [SerializeField] Image BorderImage;
     [SerializeField] Image IconBackImage;
 
+    Image nowActiveLevelStar;
+
     SkillTableData currentData;
     Action<SkillTableData, RectTransform> DataAction = null;
+    Action<RectTransform> StarCallBack = null;
 
-    public void Open(SkillTableData _data, Action<SkillTableData , RectTransform> _callBack , Sprite _borderSprite)
+    Coroutine starCoroutine = null;
+
+    public void Open(SkillTableData _data, Action<SkillTableData , RectTransform> _callBack , Action<RectTransform> _starCallBack , Sprite _borderSprite)
     {
         base.Open();
 
         currentData = _data;
         DataAction = _callBack;
+        StarCallBack = _starCallBack;
         IconBackImage.sprite = _borderSprite;
 
         SetUi();
@@ -27,16 +35,57 @@ public class UiItemSkillInfo : UIBase
 
     public void SetUi()
     {
+        if (starCoroutine != null)
+        {
+            StopCoroutine(starCoroutine);
+        }
+        starCoroutine = null;
+
         for (int i = 0; i < StarImageArr.Length; i++)
         {
             SetImgActive(StarImageArr[i], i < currentData.skilllv ? true : false);
+            StarImageArr[i].color = new Color(1,1,1,0);
         }
 
         SetIcon(IconImage, currentData.skillicon);
+
+        starCoroutine = StartCoroutine(CoStarFade());
     }
 
     public void OnClickSkillBtn()
     {
         DataAction?.Invoke(currentData , IconBackImage.GetComponent<RectTransform>());
+        StarCallBack?.Invoke(nowActiveLevelStar.rectTransform);
+    }
+
+    private IEnumerator CoStarFade()
+    {
+        yield return null;
+        nowActiveLevelStar = StarImageArr[currentData.skilllv - 1];
+        
+        bool isFadeIn = true;
+
+        while (gameObject.activeInHierarchy)
+        {
+            yield return null;
+            
+            if (nowActiveLevelStar.color.a <= 0.9f && isFadeIn)
+            {
+                nowActiveLevelStar.color = new Color(1, 1, 1, nowActiveLevelStar.color.a + 0.01f);
+            }
+            else
+            {
+                nowActiveLevelStar.color = new Color(1, 1, 1, nowActiveLevelStar.color.a - 0.01f);
+            }
+
+            if(nowActiveLevelStar.color.a <= 0)
+            {
+                isFadeIn = true;
+            }
+            else if(nowActiveLevelStar.color.a >= 0.9f)
+            {
+                isFadeIn = false;
+            }
+        }
     }
 }
