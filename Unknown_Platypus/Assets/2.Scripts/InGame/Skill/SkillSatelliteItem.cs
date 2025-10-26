9e11c250f4e052cbbcc3d019d4329ac2ca639532
kwd465 <kwd465@gmail.com>
2025-10-26 20:11:34 +0900

    위성지원 기획서에 적혀있는 버그  내용 수정
    플레이어가 바라보는 방향으로 발사하게 수정

--- Unknown_Platypus/Assets/2.Scripts/InGame/Skill/SkillSatelliteItem.cs
+++ Unknown_Platypus/Assets/2.Scripts/InGame/Skill/SkillSatelliteItem.cs
@@ -5,77 +5,78 @@ using UnityEngine;
 
 public class SkillSatelliteItem : MonoBase
 {
-
     [SerializeField]
     private Transform m_trBullet;
-    
+
     private int m_curState = 0;     //0 : 대기, 1 : 공격
     private float m_waitTime = 0;
-    private float m_attackTime = 0; 
+    private float m_attackTime = 0;
     private float m_attackDealy = 0;
     private SkillObject m_parent;
-
-    private Player m_target;
+    Vector3 dir = Vector3.zero;
+    //private Player m_target;
 
     public virtual void Open(SkillObject _parent)
     {
         base.Open();
         m_parent = _parent;
-        
     }
 
     public override void UpdateLogic()
     {
         base.UpdateLogic();
 
-        if(m_curState == 0)
+        if (m_curState == 0)
         {
             m_waitTime += Time.fixedDeltaTime;
-            if(m_waitTime >= 1.5f)
+            if (m_waitTime >= 1.5f)
             {
                 m_curState = 1;
                 m_waitTime = 0;
             }
         }
-        else if(m_curState == 1)
+        else if (m_curState == 1)
         {
             m_attackTime += Time.fixedDeltaTime;
             m_attackDealy += Time.fixedDeltaTime;
-            if(m_attackTime >= m_parent.m_duration)
+            if (m_attackTime >= m_parent.m_duration)
             {
                 m_curState = 0;
                 m_attackTime = 0;
             }
 
-            if(m_attackDealy >= 0.3f)
+            if (m_attackDealy >= 0.3f)
             {
-                
-            
-                m_target =GameUtil.GetAreaTarget(m_parent.Owner ,m_parent.m_distance ,m_parent.m_distance, false);
-                
-                if(m_target == null || m_target.getData.HP <= 0)
-                {
-                    return;
-                }
+                //m_target =GameUtil.GetAreaTarget(m_parent.Owner ,m_parent.m_distance ,m_parent.m_distance, false);
 
-                Vector3 _dir = Vector3.zero;
-                if(m_target.IsMove)
+                //if(m_target == null || m_target.getData.HP <= 0)
+                //{
+                //    return;
+                //}
+
+                //Vector3 _dir = Vector3.zero;
+                //if(m_target.IsMove)
+                //{
+                //    _dir = (m_target.inputVec - transform.position).normalized;
+                //}
+                //else
+                //{
+                //    _dir = (m_target.transform.position - transform.position).normalized;
+                //}
+
+                if (m_parent.Owner.inputVec.normalized != Vector3.zero)
                 {
-                    _dir = (m_target.inputVec - transform.position).normalized;
-                }
-                else
-                {
-                    _dir = (m_target.transform.position - transform.position).normalized;
+                    dir = m_parent.Owner.inputVec.normalized;
                 }
 
                 // 이동 방향의 각도를 구합니다.
-                float angle = Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg;
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
                 // 오브젝트의 회전 각도를 설정합니다.
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-                Effect _bullet = EffectManager.instance.Play("SatelliteBullet" , m_trBullet.position , Quaternion.AngleAxis(angle, Vector3.forward));
+                Effect _bullet = EffectManager.instance.Play("SatelliteBullet", m_trBullet.position, Quaternion.AngleAxis(angle, Vector3.forward));
                 SkillObject _obj = _bullet.GetComponent<SkillObject>();
-                _obj.Init(m_parent.SkillEffect, _target:null, m_parent.Owner, _dir);       
-                m_attackDealy = 0;      
+                _obj.Init(m_parent.SkillEffect, _target: null, m_parent.Owner, dir);
+                m_attackDealy = 0;
             }
         }
     }
